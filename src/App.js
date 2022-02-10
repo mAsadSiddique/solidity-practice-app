19a8bbeb7d23614a90a7db0d4eed79bf92a165d2
mAsadSiddique <asadsiddique.a@gmail.com>
2022-02-10 23:26:16 +0500

    Set the files export

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import "./App.css";
+import { Navbar, Services, Footer, Transaction, Welcome } from "./components/index";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome"> 
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transaction/>
+      <Footer/>
     </div>
   );
 }
